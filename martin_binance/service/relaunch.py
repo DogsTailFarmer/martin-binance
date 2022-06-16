36f2624c96347dcbd79a6b019e932c1f7f6d62bf
DogsTailFarmer <jerry.fedorenko@yahoo.com>
2022-06-16 19:47:45 +0300

    fix some sonarcloude issues

--- martin_binance/service/relaunch.py
+++ martin_binance/service/relaunch.py
@@ -14,19 +14,12 @@ __contact__ = 'https://github.com/DogsTailFarmer'
 import os
 import libtmux
 import time
-import logging
-
-logging.basicConfig(filename='./log/relaunch.log', filemode='w', level=logging.INFO)
-
 
 time.sleep(10)
-logging.info('Start relaunch')
 
 server = libtmux.Server()
 
 session = server.find_where({"session_name": "Trade"})
-
-logging.info(f"session: {session}")
 
 if session:
     for window in session.list_windows():
@@ -35,12 +28,10 @@ if session:
         pane = window.attached_pane
         if window_name == 'srv':
             pane.send_keys('./exch_srv.py', enter=True)
-            logging.info('Starting srv')
         else:
             last_state = './last_state/' + window_name.replace('-', '_').replace('/', '') + '.json'
             pair = './cli_' + window_name.replace('-', '_').replace('/', '') + '.py'
             if os.path.exists(pair) and os.path.exists(last_state):
-                logging.info(f"For {window_name} use {last_state} start {pair}")
                 pair += ' 1'
                 pane.send_keys(pair, enter=True)
         time.sleep(4)
