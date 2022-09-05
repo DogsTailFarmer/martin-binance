339f2021f012f24d6aff08430c3412d8f3118730
DanyaSWorlD <bucashechca@gmail.com>
2022-09-05 20:10:33 +0400

    Refactor telegram, add inline keyboard capability (#24)
    * Refactor telegram and add telegram inline buttons
    
    * fix style
    
    * LESS CHANGES
    
    * Add inline configurations
    
    Co-authored-by: Daniil Mirenskii <dania.mir@yandex.ru>

--- martin_binance/executor.py
+++ martin_binance/executor.py
@@ -106,6 +106,7 @@ TELEGRAM_URL = str()
 TOKEN = str()
 CHANNEL_ID = str()
 STOP_TLG = 'stop_signal_QWE#@!'
+INLINE_BOT = True
 # endregion
 
 
@@ -131,7 +132,6 @@ class Style:
 
 
 def telegram(queue_to_tlg, _bot_id) -> None:
-
     url = TELEGRAM_URL
     token = TOKEN
     channel_id = CHANNEL_ID
@@ -141,7 +141,21 @@ def telegram(queue_to_tlg, _bot_id) -> None:
     retries = Retry(total=5, backoff_factor=1, status_forcelist=[101, 111, 502, 503, 504])
     s.mount('https://', HTTPAdapter(max_retries=retries))
 
-    def requests_post(_method, _data, session):
+    def get_keyboard_markup():
+        return json.dumps({
+            "inline_keyboard": [
+                [
+                    {'text': 'status', 'callback_data': 'status_callback'},
+                    {'text': 'stop', 'callback_data': 'stop_callback'},
+                    {'text': 'end', 'callback_data': 'end_callback'}
+                ]
+            ]})
+
+    def requests_post(_method, _data, session, inline_buttons=False):
+        if INLINE_BOT and inline_buttons:
+            keyboard = get_keyboard_markup()
+            _data['reply_markup'] = keyboard
+
         _res = None
         try:
             _res = session.post(_method, data=_data)
@@ -151,37 +165,105 @@ def telegram(queue_to_tlg, _bot_id) -> None:
             print(f"Telegram: {_exc}")
         return _res
 
+    def parse_query(update_inner):
+        update_id = update_inner.get('update_id')
+        query = update_inner.get('callback_query')
+        from_id = query.get('from').get('id')
+
+        if from_id != int(channel_id):
+            return None
+
+        message = query.get('message')
+        message_id = message.get('message_id')
+        query_data = query.get('data')
+        reply_to_message = message.get('text')
+
+        command = None
+        if query_data == 'status_callback':
+            command = 'status'
+        elif query_data == 'stop_callback':
+            command = 'stop'
+        elif query_data == 'end_callback':
+            command = 'end'
+
+        requests_post(url + '/answerCallbackQuery', {'callback_query_id': query.get('id')}, s)
+
+        return {
+            'update_id': update_id,
+            'message_id': message_id,
+            'text_in': command,
+            'reply_to_message': reply_to_message
+        }
+
+    def parse_command(update_inner):
+        update_id = update_inner.get('update_id')
+        message = update_inner.get('message')
+        from_id = message.get('from').get('id')
+        if from_id != int(channel_id):
+            return None
+
+        message_id = message.get('message_id')
+        text_in = update_inner.get('message').get('text')
+
+        try:
+            reply_to_message = message.get('reply_to_message').get('text')
+        except AttributeError:
+            reply_to_message = None
+            _text = "The command must be a response to any message from a specific strategy," \
+                    " use Reply + Menu combination"
+            requests_post(method, _data={'chat_id': channel_id, 'text': _text}, session=s)
+
+        return {
+            'update_id': update_id,
+            'message_id': message_id,
+            'text_in': text_in,
+            'reply_to_message': reply_to_message
+        }
+
     def telegram_get(offset=None) -> []:
         command_list = []
         _method = url + '/getUpdates'
         _res = requests_post(_method, _data={'chat_id': channel_id, 'offset': offset}, session=s)
-        if _res and _res.status_code == 200:
-            __result = _res.json().get('result')
-            # print(f"telegram_get.result: offset: {offset}, {__result}")
-            message_id = None
-            text_in = None
-            reply_to_message = None
-            for i in __result:
-                update_id = i.get('update_id')
-                message = i.get('message')
-                if message:
-                    from_id = message.get('from').get('id')
-                    if from_id == int(channel_id):
-                        message_id = message.get('message_id')
-                        text_in = i.get('message').get('text')
-                        try:
-                            reply_to_message = i.get('message').get('reply_to_message').get('text')
-                        except AttributeError:
-                            reply_to_message = None
-                            _text = "The command must be a response to any message from a specific strategy," \
-                                    " use Reply + Menu combination"
-                            requests_post(method, _data={'chat_id': channel_id, 'text': _text}, session=s)
-                command_list.append({'update_id': update_id, 'message_id': message_id,
-                                     'text_in': text_in, 'reply_to_message': reply_to_message})
+
+        if not _res or _res.status_code != 200:
+            return command_list
+
+        __result = _res.json().get('result')
+        for result_in in __result:
+            parsed = None
+            if result_in.get('message') is not None:
+                parsed = parse_command(result_in)
+            if result_in.get('callback_query') is not None:
+                parsed = parse_query(result_in)
+            if parsed:
+                command_list.append(parsed)
+
         return command_list
 
+    def process_update(update_inner):
+        reply = update_inner.get('reply_to_message')
+
+        if not reply:
+            return
+
+        in_bot_id = reply.split('.')[0]
+        if in_bot_id != _bot_id:
+            return
+
+        try:
+            msg_in = str(update_inner['text_in']).lower().strip().replace('/', '')
+            connection_control.execute('insert into t_control values(?,?,?,?)',
+                                       (update_inner['message_id'], msg_in, in_bot_id, None))
+            connection_control.commit()
+        except sqlite3.Error as ex:
+            print(f"telegram: insert into t_control: {ex}")
+        else:
+            # Send receipt
+            post_text = f"Received '{msg_in}' command, OK"
+            requests_post(method, _data={'chat_id': channel_id, 'text': post_text}, session=s)
+
     # Set command for Telegram bot
-    _command = requests_post(url + '/getMyCommands', _data=None,  session=s)
+    _command = requests_post(url + '/getMyCommands', _data=None, session=s)
     if _command and _command.status_code == 200 and not _command.json().get('result', None):
         _commands = {
             "commands": json.dumps([
@@ -206,31 +288,17 @@ def telegram(queue_to_tlg, _bot_id) -> None:
             break
         except queue.Empty:
             # Get external command from Telegram bot
-            x = telegram_get(offset_id)
-            if x:
-                offset_id = x[-1].get('update_id')
-                offset_id += 1
-                for n in x:
-                    a = n.get('reply_to_message')
-                    if a:
-                        bot_id = a.split('.')[0]
-                        if bot_id == _bot_id:
-                            try:
-                                msg_in = str(n['text_in']).lower().strip().replace('/', '')
-                                connection_control.execute('insert into t_control values(?,?,?,?)',
-                                                           (n['message_id'], msg_in, bot_id, None))
-                                connection_control.commit()
-                            except sqlite3.Error as ex:
-                                print(f"telegram: insert into t_control: {ex}")
-                            else:
-                                # Send receipt
-                                text = f"Received '{msg_in}' command, OK"
-                                requests_post(method, _data={'chat_id': channel_id, 'text': text}, session=s)
+            updates = telegram_get(offset_id)
+            if not updates:
+                continue
+            offset_id = updates[-1].get('update_id') + 1
+            for update in updates:
+                process_update(update)
         else:
             if text and STOP_TLG in text:
                 connection_control.close()
                 break
-            requests_post(method, _data={'chat_id': channel_id, 'text': text}, session=s)
+            requests_post(method, _data={'chat_id': channel_id, 'text': text}, session=s, inline_buttons=True)
 
 
 def db_management() -> None:
