0bf8e24e1ba1bc318ecf46938c814e84a1cc66eb
DogsTailFarmer <jerry.fedorenko@yahoo.com>
2023-04-05 19:50:23 +0300

    1.2.16

--- martin_binance/executor.py
+++ martin_binance/executor.py
@@ -4,7 +4,7 @@
 __author__ = "Jerry Fedorenko"
 __copyright__ = "Copyright © 2021 Jerry Fedorenko aka VM"
 __license__ = "MIT"
-__version__ = "1.2.16b2"
+__version__ = "1.2.16"
 __maintainer__ = "Jerry Fedorenko"
 __contact__ = 'https://github.com/DogsTailFarmer'
 ##################################################################
