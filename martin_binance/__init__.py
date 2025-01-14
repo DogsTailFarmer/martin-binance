131b33db430e22f9a9b849b24c9c029afd2e3f2b
DogsTailFarmer <jerry.fedorenko@yahoo.com>
2025-01-14 20:01:12 +0300

    3.0.17rc12

--- martin_binance/__init__.py
+++ martin_binance/__init__.py
@@ -6,7 +6,7 @@ Free trading system for Binance SPOT API
 __author__ = "Jerry Fedorenko"
 __copyright__ = "Copyright © 2021 Jerry Fedorenko aka VM"
 __license__ = "MIT"
-__version__ = "3.0.17rc11"
+__version__ = "3.0.17rc12"
 __maintainer__ = "Jerry Fedorenko"
 __contact__ = "https://github.com/DogsTailFarmer"
 
